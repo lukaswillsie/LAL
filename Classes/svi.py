7d56a2347d10c0aeaeab8496fe05d162635f29e2
Vaskar <vaskar.nath@outlook.com>
2022-12-16 10:39:40 -0700

    using cuda

--- Classes/svi.py
+++ Classes/svi.py
@@ -19,7 +19,7 @@ class GaussianSVI:
     def diag_gaussian_logpdf(x, mean, log_std):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # Evaluate the density of single point on a diagonal multivariate Gaussian.
-        normal = torch.distributions.normal.Normal(mean, torch.exp(log_std)).to(device)
+        normal = torch.distributions.normal.Normal(mean.to(device), torch.exp(log_std).to(device))
         return torch.sum(normal.log_prob(x.to(device)), dim=-1)
 
     def elbo(self, logprob, num_samples, mean, log_std):
