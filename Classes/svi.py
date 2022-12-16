3309d520303b42c1806c2587a238e0bfa2db776e
Vaskar <vaskar.nath@outlook.com>
2022-12-16 10:36:01 -0700

    using cuda

--- Classes/svi.py
+++ Classes/svi.py
@@ -7,10 +7,11 @@ class GaussianSVI:
     def __init__(self, true_posterior, num_samples_per_iter) -> None:
         self.logprob = true_posterior
         self.num_samples = num_samples_per_iter
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def diag_gaussian_sample(self, num_samples, mean, log_std):
         # Take a single sample from a diagonal multivariate Gaussian.
-        return mean + torch.exp(log_std) * torch.randn(size=(num_samples, mean.shape[0]))
+        return (mean + torch.exp(log_std) * torch.randn(size=(num_samples, mean.shape[0]))).to(self.device)
         # you must use the reparameterization trick.  Also remember that
         # we are parameterizing the _log_ of the standard deviation.
 
@@ -23,7 +24,7 @@ class GaussianSVI:
     def elbo(self, logprob, num_samples, mean, log_std):
         # Single-sample Monte Carlo estimate of the variational lower bound.
         sampled_zs = self.diag_gaussian_sample(num_samples, mean, log_std)
-        return logprob(sampled_zs) - self.diag_gaussian_logpdf(sampled_zs, mean, log_std)
+        return logprob(sampled_zs) - self.diag_gaussian_logpdf(sampled_zs, mean, log_std).to(self.device)
 
     def batch_elbo(self, logprob, params, num_samples):
         # Average over a batch of random samples. 
